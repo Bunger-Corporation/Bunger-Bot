f89586d3d2272a1b4339086f8708e7036117d243
emilia222 <130674415+emilia222@users.noreply.github.com>
2023-08-24 20:07:20 -0700

    Question, eightball

--- cogs/fun.py
+++ cogs/fun.py
@@ -40,3 +40,23 @@ class fun_cog(commands.Cog):
             return
 
         await ctx.send(text)
+
+    @commands.command()
+    async def question(self, ctx, *, question = ""):
+        responses = ["yes", "no", "maybe"]
+
+        if len(question) == 0:
+            await ctx.send(embed=embeds.craft("No question provided!", discord.Color.red()))
+            return
+
+        await ctx.send(embed=embeds.craft(f"Question: {question}\nAnswer: {random.choice(responses)}"))
+
+    @commands.command()
+    async def eightball(self, ctx, *, question = ""):
+        responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy, try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no",  "Outlook not so good", "Very doubtful"]
+        
+        if len(question) == 0:
+            await ctx.send(embed=embeds.craft("No question provided!", discord.Color.red()))
+            return
+
+        await ctx.send(embed=embeds.craft(f"Eightball question: {question}\nAnswer: {random.choice(responses)}"))
